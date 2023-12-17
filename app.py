9650bdd4b6922620e8cb32006c8741573adffa55
AnindoAkash <120242075+AnindoAkash@users.noreply.github.com>
2023-12-17 18:46:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-# https://youtu.be/pI0wQbJwIIs
-"""
-
-Werkzeug provides a bunch of utilities for developing WSGI-compliant applications. 
-These utilities do things like parsing headers, sending and receiving cookies, 
-providing access to form data, generating redirects, generating error pages when 
-there's an exception, even providing an interactive debugger that runs in the browser. 
-Flask then builds upon this foundation to provide a complete web framework.
-"""
-
 from flask import Flask, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
 from main import getPrediction
@@ -20,21 +10,12 @@ UPLOAD_FOLDER = 'static/images/'
 app = Flask(__name__, static_folder="static")
 
 #Add reference fingerprint. 
-#Cookies travel with a signature that they claim to be legit. 
-#Legitimacy here means that the signature was issued by the owner of the cookie.
-#Others cannot change this cookie as it needs the secret key. 
-#It's used as the key to encrypt the session - which can be stored in a cookie.
-#Cookies should be encrypted if they contain potentially sensitive information.
 app.secret_key = "secret key"
 
 #Define the upload folder to save images uploaded by the user. 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#Define the route to be home. 
-#The decorator below links the relative route of the URL to the function it is decorating.
-#Here, index function is with '/', our root directory. 
-#Running the app sends us to index.html.
-#Note that render_template means it looks for the file in the templates folder. 
+#Define the route to be home.  
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -58,7 +39,3 @@ def submit_file():
             full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             flash(full_filename)
             return redirect('/')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
